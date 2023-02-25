344280b26a274365b2f326fe1dd7ba7a421c903c
DylanVallejo <dylan1811vallejo@gmail.com>
2023-02-24 23:01:02 -0500

    adding dependencies bootstrap sweet lert react router sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+// import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      .container
     </div>
   );
 }
